4f0c3153471a40d288a3bc91bdf84ccbe3caf27d
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-09 08:31:15 +0530

    Update

--- SchoolSystem/SchoolSystem/SampleAdd.cs
+++ SchoolSystem/SchoolSystem/SampleAdd.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace SchoolSystem
 {
-    public partial class SampleAdd : Form
+    public partial class SampleAdd : Sample
     {
         public SampleAdd()
         {
