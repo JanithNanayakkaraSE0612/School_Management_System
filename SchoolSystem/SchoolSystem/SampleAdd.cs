f43a96aecbaa133a8b8c8ad5eedd0035eb7ca252
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-09 08:26:30 +0530

    Sample.cs Complete

--- SchoolSystem/SchoolSystem/SampleAdd.cs
+++ SchoolSystem/SchoolSystem/SampleAdd.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SchoolSystem
 {
-    public partial class Form1 : Form
+    public partial class SampleAdd : Form
     {
-        public Form1()
+        public SampleAdd()
         {
             InitializeComponent();
         }
