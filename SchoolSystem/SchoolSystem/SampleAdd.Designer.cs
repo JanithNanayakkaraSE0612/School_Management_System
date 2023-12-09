1fd3c457ee64668a055802e943dd7eef963d43fe
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-09 08:57:31 +0530

    Change the panel Name

--- SchoolSystem/SchoolSystem/SampleAdd.Designer.cs
+++ SchoolSystem/SchoolSystem/SampleAdd.Designer.cs
@@ -28,44 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.Toppanel = new System.Windows.Forms.Panel();
+            this.Bottompanel = new System.Windows.Forms.Panel();
+            this.gunaControlBox1 = new Guna.UI.WinForms.GunaControlBox();
+            this.Toppanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // Toppanel
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(74)))));
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 100);
-            this.panel1.TabIndex = 0;
+            this.Toppanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(74)))));
+            this.Toppanel.Controls.Add(this.gunaControlBox1);
+            this.Toppanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Toppanel.Location = new System.Drawing.Point(0, 0);
+            this.Toppanel.Name = "Toppanel";
+            this.Toppanel.Size = new System.Drawing.Size(800, 100);
+            this.Toppanel.TabIndex = 0;
             // 
-            // panel2
+            // Bottompanel
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(74)))));
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 350);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 100);
-            this.panel2.TabIndex = 1;
+            this.Bottompanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(74)))));
+            this.Bottompanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Bottompanel.Location = new System.Drawing.Point(0, 350);
+            this.Bottompanel.Name = "Bottompanel";
+            this.Bottompanel.Size = new System.Drawing.Size(800, 100);
+            this.Bottompanel.TabIndex = 1;
+            // 
+            // gunaControlBox1
+            // 
+            this.gunaControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gunaControlBox1.AnimationHoverSpeed = 0.07F;
+            this.gunaControlBox1.AnimationSpeed = 0.03F;
+            this.gunaControlBox1.IconColor = System.Drawing.Color.Black;
+            this.gunaControlBox1.IconSize = 15F;
+            this.gunaControlBox1.Location = new System.Drawing.Point(752, 3);
+            this.gunaControlBox1.Name = "gunaControlBox1";
+            this.gunaControlBox1.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.gunaControlBox1.OnHoverIconColor = System.Drawing.Color.White;
+            this.gunaControlBox1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.gunaControlBox1.TabIndex = 0;
             // 
             // SampleAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Bottompanel);
+            this.Controls.Add(this.Toppanel);
             this.Name = "SampleAdd";
             this.Text = "SampleAdd";
+            this.Toppanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel Toppanel;
+        private System.Windows.Forms.Panel Bottompanel;
+        private Guna.UI.WinForms.GunaControlBox gunaControlBox1;
     }
 }
