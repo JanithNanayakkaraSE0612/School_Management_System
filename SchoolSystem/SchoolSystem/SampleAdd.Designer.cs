94a022d777937b2f49dcb1ae3a2bd58f09fec09f
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-10 02:11:58 +0530

    Update

--- SchoolSystem/SchoolSystem/SampleAdd.Designer.cs
+++ SchoolSystem/SchoolSystem/SampleAdd.Designer.cs
@@ -28,15 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SampleAdd));
             this.Toppanel = new System.Windows.Forms.Panel();
-            this.Bottompanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.gunaControlBox1 = new Guna.UI.WinForms.GunaControlBox();
+            this.Bottompanel = new System.Windows.Forms.Panel();
+            this.gunaButton1 = new Guna.UI.WinForms.GunaButton();
             this.Toppanel.SuspendLayout();
+            this.Bottompanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // Toppanel
             // 
             this.Toppanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(74)))));
+            this.Toppanel.Controls.Add(this.label1);
             this.Toppanel.Controls.Add(this.gunaControlBox1);
             this.Toppanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.Toppanel.Location = new System.Drawing.Point(0, 0);
@@ -44,14 +49,15 @@
             this.Toppanel.Size = new System.Drawing.Size(800, 100);
             this.Toppanel.TabIndex = 0;
             // 
-            // Bottompanel
+            // label1
             // 
-            this.Bottompanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(74)))));
-            this.Bottompanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Bottompanel.Location = new System.Drawing.Point(0, 350);
-            this.Bottompanel.Name = "Bottompanel";
-            this.Bottompanel.Size = new System.Drawing.Size(800, 100);
-            this.Bottompanel.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Header";
             // 
             // gunaControlBox1
             // 
@@ -68,6 +74,40 @@
             this.gunaControlBox1.Size = new System.Drawing.Size(45, 29);
             this.gunaControlBox1.TabIndex = 0;
             // 
+            // Bottompanel
+            // 
+            this.Bottompanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(74)))));
+            this.Bottompanel.Controls.Add(this.gunaButton1);
+            this.Bottompanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Bottompanel.Location = new System.Drawing.Point(0, 350);
+            this.Bottompanel.Name = "Bottompanel";
+            this.Bottompanel.Size = new System.Drawing.Size(800, 100);
+            this.Bottompanel.TabIndex = 1;
+            // 
+            // gunaButton1
+            // 
+            this.gunaButton1.Animated = true;
+            this.gunaButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaButton1.AnimationSpeed = 0.03F;
+            this.gunaButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaButton1.Image = ((System.Drawing.Image)(resources.GetObject("gunaButton1.Image")));
+            this.gunaButton1.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton1.Location = new System.Drawing.Point(41, 20);
+            this.gunaButton1.Name = "gunaButton1";
+            this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton1.OnHoverImage = null;
+            this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton1.Size = new System.Drawing.Size(160, 42);
+            this.gunaButton1.TabIndex = 0;
+            this.gunaButton1.Text = "gunaButton1";
+            // 
             // SampleAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
@@ -78,6 +118,8 @@
             this.Name = "SampleAdd";
             this.Text = "SampleAdd";
             this.Toppanel.ResumeLayout(false);
+            this.Toppanel.PerformLayout();
+            this.Bottompanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -87,5 +129,7 @@
         private System.Windows.Forms.Panel Toppanel;
         private System.Windows.Forms.Panel Bottompanel;
         private Guna.UI.WinForms.GunaControlBox gunaControlBox1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI.WinForms.GunaButton gunaButton1;
     }
 }
